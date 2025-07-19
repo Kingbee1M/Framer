b0eb27af46dd57d5b10679854bb706d58bdb7c3f
Kingbee1M <160544114+Kingbee1M@users.noreply.github.com>
2025-07-19 21:38:56 +0100

    fixed a bug

--- src/app/components/SignUp.tsx
+++ src/app/components/SignUp.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export default function SignUp (){
     return (
-        <div className="relative w-full flex justify-center items-center bg-gradient-to-b from-secGradStart from-30% to-secGradEnd to-70% py-16 lg:py-32">
+        <div className="relative w-full flex justify-center items-center bg-gradient-to-b from-secGradStart from-30% to-secGradEnd to-70% py-16 lg:py-32 overflow-hidden ">
             <Image src="/images/emojistar 1.svg" alt="emojistar" width={100} height={100} className="w-3/4 hidden md:flex lg:hidden" />
             <Image src="/images/emojistar wide.svg" alt="emojistar" width={100} height={100} className="w-full hidden lg:flex lg:absolute lg:w-1/4 lg:bottom-[100px] lg:right-[1100px]" />
             <div className="w-4/5 font-main flex flex-col justify-center items-center gap-6 ">
